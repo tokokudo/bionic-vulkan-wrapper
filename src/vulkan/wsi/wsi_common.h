5d09a99ef9bce622879c4d118f0f5692c068871d
Toko kudu <joaobat0102@gmail.com>
2025-07-24 21:15:19 -0300

    Update wsi_common.h

--- src/vulkan/wsi/wsi_common.h
+++ src/vulkan/wsi/wsi_common.h
@@ -274,7 +274,7 @@ struct wsi_device {
    WSI_CB(MapMemory);
    WSI_CB(UnmapMemory);
    WSI_CB(WaitSemaphores);
-#ifdef __TERMUX__
+#ifdef __ANDROID__
    WSI_CB(GetMemoryAndroidHardwareBufferANDROID);
    WSI_CB(GetAndroidHardwareBufferPropertiesANDROID);
 #endif
